b20d90a6d30258e3a98964768bd423ca85f05662
Marko Stokic <m.markostokic@gmail.com>
2025-12-03 11:54:08 +0100

    Changed green and communal class to match parent class

---
+++ src/model/CommunalActivity.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class CommunalActivity extends Activity
+{
+  private ArrayList<Resident> participants;
+  private boolean isPointAssign;
+
+  public CommunalActivity(String title, String description, int points)
+  {
+    super(title, description, points);
+    isPointAssign = false;
+  }
+
+  public void assignPointsToParticipants() {
+    if(!isPointAssign)
+    {
+      isPointAssign = true;
+      for (int i = 0; i < participants.size(); i++)
+      {
+        participants.get(i).addPoints(points);
+      }
+    }
+  }
+
+}
--- src/model/GreenActivity.java
+++ src/model/GreenActivity.java
@@ -2,48 +2,11 @@ package model;
 
 import java.util.UUID;
 
-public class GreenActivity
+public class GreenActivity extends Activity
 {
-  private final String ID;
-  private String title = "";
-  private String description = "";
-  private int greenPoint = 0;
 
-  public GreenActivity(String title, String description, int greenPoint)
+  public GreenActivity(String title, String description, int points)
   {
-    ID = UUID.randomUUID().toString();
-    this.title = title;
-    this.description = description;
-    this.greenPoint = greenPoint;
-  }
-
-  public String getTitle()
-  {
-    return title;
-  }
-
-  public void setTitle(String title)
-  {
-    this.title = title;
-  }
-
-  public String getDescription()
-  {
-    return description;
-  }
-
-  public void setDescription(String description)
-  {
-    this.description = description;
-  }
-
-  public int getGreenPoint()
-  {
-    return greenPoint;
-  }
-
-  public void setGreenPoint(int greenPoint)
-  {
-    this.greenPoint = greenPoint;
+    super(title, description, points);
   }
 }
